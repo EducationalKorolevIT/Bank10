00e8aaa84bb5dd9b9a5c512e0631363ca938ffbb
dex22044 <dex22044@yandex.ru>
2020-01-28 17:47:17 +0300

    Обновил меню

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -11,28 +11,38 @@ namespace Bank
             "Заморозить счет",
             "",
             "Внести",
-            "Снять"
+            "Снять",
+            "Выход"
         };
 
         static public int selection;
         static public int level;
+        static char c='w';
+        static bool work = true;
         static void Main(string[] args)
         {
-            while (true)
+            while (work)
             {
-                char c = Console.ReadKey(true).KeyChar; //Читаем кнопку
-
-
                 Console.Clear(); //Очищаем консоль
-                Console.WriteLine((int)c);
+
+                for (int i = 0; i < options.Length; i++) //Выводим все пункты меню
+                {
+                    if (selection != i) Console.WriteLine("  " + options[i]);
+                    else Console.WriteLine("> " + options[i]);
+                }
+
+                c = Console.ReadKey(true).KeyChar; //Читаем кнопку
+
+                //Логика визу
 
                 //Изменяем выделенный пункт меню
                 if (c == 'w' || c == 'W') selection--;
                 else if (c == 's' || c == 'S') selection++;
-                else if ((int)c == 13) {
+                else if ((int)c == 13)
+                {
                     EnterSelection(selection);
                 }
-                
+
 
                 if (selection < 0) selection = options.Length - 1;
                 else if (selection > options.Length - 1) selection = 0;
@@ -40,19 +50,19 @@ namespace Bank
 
                 Console.BackgroundColor = ConsoleColor.DarkGreen; //Цвет фона
                 Console.ForegroundColor = ConsoleColor.White; //Цвет символов
-
-                for(int i = 0; i < options.Length; i++) //Выводим все пункты меню
-                {
-                    if (selection != i) Console.WriteLine("  "+options[i]);
-                    else Console.WriteLine("> " + options[i]);
-                }
             }
         }
 
         static void EnterSelection(int selection) {
-            switch (selection) { 
-            
+            switch (selection) {
+                case 0:Console.WriteLine("Выбрано открытие счета");Console.ReadKey();break;
+                case 1:Console.WriteLine("Выбрано закрытие счета");Console.ReadKey();break;
+                case 2:Console.WriteLine("Выбрана заморозка счета");Console.ReadKey();break;
+                case 4:Console.WriteLine("Выбрано внесение");Console.ReadKey();break;
+                case 5:Console.WriteLine("Выбрано снятие");Console.ReadKey();break;
+                case 6:work = false;break;
             }
+            Console.Clear();
         }
     }
 }
