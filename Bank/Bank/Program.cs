d6fc10ef00bcacd29578daba4ce783743bdf33d0
sashazvereva <sasazvereva15@gmail.com>
2020-01-30 18:05:48 +0300

    Добавила счёта
    Добавила открытие и просмотр счетов

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Bank
 {
@@ -10,13 +9,11 @@ namespace Bank
             "Открыть счет",
             "Закрыть счет",
             "Заморозить счет",
-            "",
+            "Показать счета",
             "Внести",
             "Снять",
             "Выход"
         };
-
-        static List<Account> accounts=new List<Account>();
 
         static public int selection;
         static public int level;
@@ -60,29 +57,48 @@ namespace Bank
             Console.Clear();
             switch (selection)
             {
-                case 0: Console.WriteLine("Выбрано открытие счета"); openAccount(); Console.ReadKey(); break;
-                case 1: Console.WriteLine("Выбрано закрытие счета"); Console.ReadKey(); break;
-                case 2: Console.WriteLine("Выбрана заморозка счета"); Console.ReadKey(); break;
-                case 4: Console.WriteLine("Выбрано внесение"); Console.ReadKey(); break;
-                case 5: Console.WriteLine("Выбрано снятие"); Console.ReadKey(); break;
-                case 6: work = false; break;
+                case 0: 
+                    { 
+                        Console.WriteLine("Выбрано открытие счета");
+                        Score.OpenScore();
+                        Console.ReadKey(); 
+                    } break;
+                case 1:
+                    {
+                        Console.WriteLine("Выбрано закрытие счета"); 
+                        Console.ReadKey();
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("Выбрана заморозка счета");
+                        Console.ReadKey();
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("Список счетов");
+                        Score.ViewScore();
+                        Console.ReadKey();
+                    }
+                    break;
+                case 4:
+                    { 
+                        Console.WriteLine("Выбрано внесение"); 
+                        Console.ReadKey(); 
+                    }
+                    break;
+                case 5:
+                    {
+                        Console.WriteLine("Выбрано снятие");
+                        Console.ReadKey();
+                    }
+                    break;
+                case 6: 
+                    {
+                        work = false; 
+                        }break;
             }
-        }
-
-        static void openAccount()
-        {
-            Pasport pas;
-            Console.WriteLine("Введите ФИО:");
-            string names = Console.ReadLine();
-            Console.WriteLine("Введите дату рождения:");
-            string bday = Console.ReadLine();
-            Console.WriteLine("Введите дату пароль:");
-            string pass=Console.ReadLine();
-            Pasport.birthDate bdate = new Pasport.birthDate();
-            bdate.fromString(bday);
-            pas = new Pasport(names.Split(' ')[0], names.Split(' ')[1], names.Split(' ')[2], bdate);
-
-            accounts.Add(new Account(pas, pass, "RUB", new Random().Next(0, 100000)));
         }
     }
 }
---
+++ Bank/Bank/Score.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    enum Currency
+    {
+        EUR,
+        RUB,
+        USD
+    }
+    //валюта
+
+    class Score
+    {
+        double Money;
+        Currency Valute;
+        int Id;
+        public static int LastId = 0;
+
+        public static List<Score> Scores = new List<Score>();
+
+        public Score()
+        {
+            Console.Write("Введите тип валюты (1 - EUR, 2 - RUB, 3 - USD):");
+            int Currency = Convert.ToInt32(Console.ReadLine());
+            if (Currency == 0)
+                Valute = Bank.Currency.EUR;
+            if (Currency == 1)
+                Valute = Bank.Currency.RUB;
+            if (Currency == 2)
+                Valute = Bank.Currency.USD;
+            Console.Write("Введите количество денежных средств:");
+            Money = Convert.ToDouble(Console.ReadLine());
+            Id = ++LastId;
+        }
+
+        public void PrintScore() {
+            Console.WriteLine("Номер счёта: " + Id);
+            Console.WriteLine("Средств на счету: " + Money);
+            if(Valute == Currency.EUR)
+                Console.WriteLine("Используемая валюта : EUR");
+            if (Valute == Currency.RUB)
+                Console.WriteLine("Используемая валюта : RUB");
+            if (Valute == Currency.USD)
+                Console.WriteLine("Используемая валюта : USD");
+        }
+
+        public static void OpenScore()
+        {
+            Scores.Add(new Score());
+            Console.WriteLine("Введите любую клавишу для продолжения...");
+        }
+        public static void ViewScore()
+        {
+            foreach (Score sc in Scores) {
+                Console.WriteLine();
+                sc.PrintScore();
+            }
+            Console.WriteLine("Введите любую клавишу для продолжения...");
+        }
+
+
+    }
+}
